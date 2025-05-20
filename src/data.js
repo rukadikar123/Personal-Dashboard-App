5f7d704ecaa4760347e9bae2f2223862481cc507
siddharth rukadikar <siddharthrukadikar@gmail.com>
2025-05-20 20:33:10 +0530

    add data in data.js file

---
+++ src/data.js
@@ -0,0 +1,66 @@
+export const profile = {
+  name: "Siddharth Rukadikar",
+  bio: "Web Developer passionate about building web apps.",
+  photo: "../public/Photo (1).jpg",
+  contact: {
+    email: "siddharthrukadikar@gmail.com",
+    github: "https://github.com/rukadikar123",
+    linkedin: "https://www.linkedin.com/in/siddharth-rukadikar18361a168/",
+  },
+};
+
+export const education = [
+  {
+    title: "B.Tech in Mechanical Engineering",
+    institution: "Sharad Institute of Technology College of Engineering",
+    Percentage: "81.42 %",
+    year: "2019 - 2022",
+  },
+  {
+    title: "Diploma in Mechanical Engineering",
+    institution: "Sharad Institute of Technology College of Engineering",
+    Percentage: "70.43 %",
+    year: "2016 - 2019",
+  },
+];
+
+export const experience = [
+  {
+    Internship: [
+      {
+        title: "Intern",
+        company: "Carline Solutions",
+        duration: "Jan 2022 - Jun 2022",
+        description: "2D drafting of 3d models using catia software.",
+      },
+    ],
+    Projects: [
+      {
+        title: "Mern Note App",
+        techStack: "MongoDb, Express.Js, React.js, Node.js ",
+        Github: "https://github.com/rukadikar123/MERN-Note-app",
+        liveLink: "https://mern-note-app-1-1cgt.onrender.com/login",
+      },
+      {
+        title: "E-Commerce App",
+        techStack: "React.js, readux, Firebase",
+        Github:
+          "https://github.com/rukadikar123/FSJS-For-Practice/tree/main/reactPractice/E-shop",
+        liveLink: "https://e-commerce-apk3.netlify.app/",
+      },
+    ],
+  },
+];
+
+export const skills = [
+  "React",
+  "Node.js",
+  "Express",
+  "MongoDB",
+  "Tailwind CSS",
+  "Git",
+  "Github",
+];
+
+
+export const Certification = ["Full Stack JavaScript Bootcamp 2.0 at iNeuron.ai"];
