cf6e91d00357620e4546d05561d82b151af796ad
siddharth rukadikar <siddharthrukadikar@gmail.com>
2025-05-22 18:53:36 +0530

    refactor: enhance structure and comments in Experience and Extras components

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { education, experience } from "../data";
 
+// This component displays the Education, Internship Experience, and Projects
 function Experience() {
   return (
     <section className="p-6 animate-fadeIn bg-white rounded-lg shadow-md">
-      <h2 className="md:text-3xl text-2xl font-semibold text-gray-800 mb-4">Education</h2>
+      {/* Education Section */}
+      <h2 className="md:text-3xl text-2xl font-semibold text-gray-800 mb-4">
+        Education
+      </h2>
       <hr />
       {education.map((item, id) => (
         <div key={id} className="mb-3 bg-gray-50 rounded-lg shadow-md p-4">
@@ -14,6 +18,7 @@ function Experience() {
           <p className="md:text-sm text-xs text-gray-600">{item.year}</p>
         </div>
       ))}
+      {/* Internship/Experience Section */}
       <h2 className="md:text-3xl text-2xl font-semibold mt-6 mb-4 ">
         Experience/Internships
       </h2>
@@ -27,10 +32,14 @@ function Experience() {
           <p className="md:text-md text-sm">{item.description}</p>
         </div>
       ))}
+      {/* Projects Section */}
       <h2 className="text-2xl md:text-3xl font-semibold mt-6 mb-4">Projects</h2>
       <hr />
       {experience.Projects.map((item, id) => (
-        <div key={id} className="mb-3 bg-gray-50 rounded-lg flex flex-col gap-1 shadow-md p-4">
+        <div
+          key={id}
+          className="mb-3 bg-gray-50 rounded-lg flex flex-col gap-1 shadow-md p-4"
+        >
           <h3 className="text-lg font-bold mb-2">{item.title}</h3>
           <p className="md:text-md text-sm text-gray-900">{item.techStack}</p>
           <a
--- src/components/Extras.jsx
+++ src/components/Extras.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Certification } from "../data";
 
+
+// This component displays certifications
 function Extras() {
   return (
     <section className="p-6 animate-fadeIn mt-6 bg-white rounded-lg shadow-md">
-      <h2 className="text-2xl font-semibold mb-4 text-gray-800">Hobbies</h2>
+      {/* Section Title */} 
+      <h2 className="text-2xl font-semibold mb-4 text-gray-800">Certification</h2>
+       {/* List of Certifications  */}
       <ul className="list-disc list-inside text-gray-700">
         {Certification.map((item, id) => (
           <li key={id}>{item}</li>
